bb872ee6933961174339b29b881660d0a2bf466b
SoniaHdez <sonia.hernandez@g-tec.io>
2019-01-25 11:03:37 -0600

    test

--- pages/articles/_id/index.vue
+++ pages/articles/_id/index.vue
@@ -19,34 +19,34 @@
       
       
       <h2>ID:</h2>
-      <p> {{articlesData.ID}}</p>
+      <p> {{articlesData.id}}</p>
       
       <h2>Título:</h2>
-      <p> {{articlesData.ARTICLE.Title}}</p>
+      <p> {{articlesData.article.title}}</p>
      
       <h2>Autor:</h2>
-      <p> {{articlesData.ARTICLE.Author}}</p>
-      <p> {{articlesData.IMG.Author}}</p>
+      <p> {{articlesData.author}}</p>
+      <p> {{articlesData.s3Dir}}</p>
       
       <h2>Fecha:</h2>
-      <p> {{articlesData.ARTICLE.DT}}</p>
-      <p> {{articlesData.IMG.DT}}</p>
+      <p> {{articlesData.dt}}</p>
+      <p> {{articlesData.s3Dir}}</p>
      
       <h2>Localización</h2>
-      <p> {{articlesData.ARTICLE.Location}}</p>
-      <p> {{articlesData.IMG.Location}}</p>
+      <p> {{articlesData.location}}</p>
+      <p> {{articlesData.s3Dir}}</p>
       
       <h2>Estado de publicación:</h2>
-      <p> {{articlesData.ARTICLE.PublishStatus}}</p>
+      <p> {{articlesData.publishStatus}}</p>
       
       <h2>Sección:</h2>
-      <p> {{articlesData.ARTICLE.Section}}</p>
+      <p> {{articlesData.article.section}}</p>
       
       <h2>Etiquetas:</h2>
-      <p> {{articlesData.ARTICLE.Tags}}</p>
+      <p> {{articlesData.article.tags}}</p>
       
       <h2>Contenido:</h2>
-      <p> {{articlesData.ARTICLE.Content}}</p>
+      <p> {{articlesData.article.content}}</p>
       
       <br>
     </form>
@@ -80,23 +80,20 @@ export default {
 //Build an object with the data from the form, return it as articleData
     buildJSON: function() {
       var articleData = {
-        ID:               document.getElementById("id").value,
-        ARTICLE: {
-          Author:         document.getElementById("author").value,
-          Content:        document.getElementById("content").value,
-          DT:             document.getElementById("date").value,
-          Location:       document.getElementById("location").value,
-          PublishStatus:  document.getElementById("status").value,
-          Section:        document.getElementById("section").value,
-          Tags:           document.getElementById("tags").value,
-          Title:          document.getElementById("title").value
+        id:             document.getElementById("id").value,
+        dt:             document.getElementById("date").value,
+        author:         document.getElementById("author").value,
+        location:       document.getElementById("location").value,
+        publishStatus:  document.getElementById("status").value,
+        s3Dir: "Test dir",
+        
+        article: {
+          content:        document.getElementById("content").value,    
+          section:        document.getElementById("section").value,
+          tags:           document.getElementById("tags").value,
+          title:          document.getElementById("title").value,
         },
-        IMG: {
-          Author: "Test Author",
-          DT: "Date Test",
-          Location: "Location Test",
-          S3DIR: "Test dir"
-        }
+
       };
       return articleData;
     }
@@ -139,7 +136,7 @@ form {
   border: 2px solid slategray;
   margin: 20px;
   align-self: auto;
-  width: auto;
+  width: 100%;
   font-family: 'Lucida Sans';
   color: grey;
 
